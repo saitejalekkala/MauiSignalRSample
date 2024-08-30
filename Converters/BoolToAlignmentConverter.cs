629b06403a67c3c9cc87091065a86c15a49101d7
saitejalekkala <saitejalekkala05@gmail.com>
2024-08-30 12:34:26 +0530

    Boolean-Converters

---
+++ Converters/BoolToAlignmentConverter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace MauiSignalRSample.Converters
+{
+    public class BoolToAlignmentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isSent)
+            {
+                return isSent ? LayoutOptions.End : LayoutOptions.Start;
+            }
+            return LayoutOptions.Start;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/BoolToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace MauiSignalRSample.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isSent)
+            {
+                return isSent ? Color.FromArgb("#10331a") : Color.FromArgb("#3d403d");
+            }
+            return Color.FromArgb("#3d403d");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
